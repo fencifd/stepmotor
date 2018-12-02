4aafa4ff9c99c19383407a0ceecbe9eb128f7c66
fencifd <fencifd@gmail.com>
2018-12-02 21:52:24 +0300

    Add files via upload

---
+++ Functions.ino
@@ -0,0 +1,57 @@
+//Function to control each pin of stepper motor
+
+void Step_A(){
+  digitalWrite(pin1, HIGH);//turn on coil 1 
+  digitalWrite(pin2, HIGH); 
+  digitalWrite(pin3, LOW); 
+  digitalWrite(pin4, LOW); 
+}
+void Step_B(){
+  digitalWrite(pin1, LOW); 
+  digitalWrite(pin2, HIGH);//turn on coil 2
+  digitalWrite(pin3, HIGH); 
+  digitalWrite(pin4, LOW); 
+}
+void Step_C(){
+  digitalWrite(pin1, LOW); 
+  digitalWrite(pin2, LOW); 
+  digitalWrite(pin3, HIGH); //turn on coil 3
+  digitalWrite(pin4, HIGH); 
+}
+void Step_D(){
+  digitalWrite(pin1, HIGH); 
+  digitalWrite(pin2, LOW); 
+  digitalWrite(pin3, LOW); 
+  digitalWrite(pin4, HIGH); //turn on coil 4
+}
+void step_OFF(){
+  digitalWrite(pin1, LOW); //power all coils down
+  digitalWrite(pin2, LOW); 
+  digitalWrite(pin3, LOW); 
+  digitalWrite(pin4, LOW); 
+}
+
+//these functions run the above configurations in forward and reverse order
+//the direction of a stepper motor depends on the order in which the coils are turned on.
+void forward(){//one tooth forward
+  Step_A();
+  delay(delaytime);
+  Step_B();
+  delay(delaytime);
+  Step_C();
+  delay(delaytime);
+  Step_D();
+  delay(delaytime);
+}
+
+void backward(){//one tooth backward
+  Step_C();
+  delay(delaytime);
+  Step_B();
+  delay(delaytime);
+  Step_A();
+  delay(delaytime);
+  Step_D();
+  delay(delaytime);
+}
+
---
+++ PasoPaso_App.ino
@@ -0,0 +1,67 @@
+//******************************************************************************
+//------------------------- www.electroingenio.com -----------------------------
+//******************************************************************************
+//***************************    PROGRAMED BY   ********************************
+//**************************** MAURICIO HIDALGO ********************************
+//******************************************************************************
+
+
+#define pin1  8//these are the Arduino pins that we use to activate coils 1-4 of the stepper motor
+#define pin2  9
+#define pin3  10
+#define pin4  11
+#define delaytime 2   //delay time in ms to control the stepper motor delaytime.
+                      //Our tests showed that 8 is about the fastest that can yield reliable operation w/o missing steps
+
+char unChar;
+String readString; //Asignamos la palabra readString a una variable tipo cadena
+float numberOfSteps = 0;
+int datoserial = 0;
+
+void setup() {
+  pinMode(pin1, OUTPUT);
+  pinMode(pin2, OUTPUT); 
+  pinMode(pin3, OUTPUT); 
+  pinMode(pin4, OUTPUT);
+  Serial.begin(9600); //initialize serial comunication
+}
+
+void loop() {
+  while (Serial.available()) { //Se verifica la disponibilidad del puero serial 
+    delay(3);                  // a traves de un loop
+    char c = Serial.read();  // Se leen los caracteres que ingresan por el puerto
+    readString += c;         //se acumulan cada uno de los caracteres en readString
+  }
+ 
+  if (readString.length() >0) { //Se verifica que la variable contenga informacion
+  
+   Serial.println(readString.toInt());  //se envia lo que tenga la variable
+    datoserial = readString.toInt();
+    
+  }
+  while(datoserial>numberOfSteps){
+             
+         backward();
+         delayMicroseconds(40);
+         numberOfSteps = numberOfSteps + 1;
+    
+  }
+ while(datoserial<numberOfSteps){
+         
+          forward();
+          delayMicroseconds(40);
+          numberOfSteps = numberOfSteps - 1;
+    
+  }
+  
+  if (readString == "SET"){
+    numberOfSteps = 0;
+    datoserial = 0;
+    }
+ 
+  readString=""; // Se limpia la variable
+}
+
+
+
+
